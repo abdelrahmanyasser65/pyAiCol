cbe6c348d6e10fefc754618e04c20fd2fa66fccf
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-05-17 06:03:12 +0300

    fun_numpy

---
+++ numpy.py
@@ -0,0 +1,37 @@
+from numpy import*
+
+#round
+# a=round(3.634)
+# print(a)
+
+# b=round(3.644,1)
+# print(b)
+
+# c=round(3.655,2)
+# print(c)
+
+# n=round(3.4)
+# print(n)
+
+
+
+#ceil and floor
+
+# a=ceil(3.65) 
+# print(a)   =>4.0
+
+# b=floor(3.65)
+# print(b)  =>3.0
+
+
+#mod
+
+# a=mod(13,6)
+# print(a)   =>1
+
+#pow
+# a=pow(5,2)
+# print(a)
+
+
+#RANDOM
